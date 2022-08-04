25b563ab2cb4d2b0df88870dde87f122d8f4985e
Joao Pedro <paivapedro16.pp@gmail.com>
2022-08-03 22:43:09 -0300

    Debugando

--- labecommecer/src/App.js
+++ labecommecer/src/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import { Header } from './components/header/Header';
-import styled from 'styled-component';
-import './App.css';
+import { Footer} from './components/footer/Footer';
+import { Filtro } from './components/filtro/Filtro';
+import { Carrinho } from './components/carrinho/Carrinho';
+import styled from 'styled-components';
+import { MenuCentral, Imagem, Botao, Card2, Titulo, Preco } from './components/lista/Styled';
+
+import camisas1 from './components/img/1.jpg';
+import camisas2 from './components/img/2.jpg';
+import camisas3 from './components/img/3.jpg';
+import camisas4 from './components/img/4.jpg';
+import camisas5 from './components/img/5.jpg';
+import camisas6 from './components/img/6.jpg';
+import camisas7 from './components/img/7.jpg';
+import camisas8 from './components/img/8.jpg';
+import camisas9 from './components/img/9.jpg';
+import camisas10 from './components/img/10.jpg';
 
 
 const Principal = styled.div`
@@ -20,10 +34,200 @@ const Principal = styled.div`
 `
 
 export default class App extends Component {
-  render() {
-    return (
-      <Header />
-    )
+  state = {
+    camisas: [
+      {
+        id: 1,
+        name: "Camisa 1",
+        value: 39.99,
+        imageUrl: camisas1,
+      },
+      {
+        id: 2,
+        name: "Camisa 2",
+        value: 44.99,
+        imageUrl: camisas2,
+      },
+      {
+        id: 3,
+        name: "Camisa 3",
+        value: 49.99,
+        imageUrl: camisas3,
+      },
+      {
+        id: 4,
+        name: "Camisa 4",
+        value: 59.99,
+        imageUrl: camisas4,
+      },
+      {
+        id: 5,
+        name: "Camisa 5",
+        value: 69.99,
+        imageUrl: camisas5,
+      },
+      {
+        id: 6,
+        name: "Camisa 6",
+        value: 79.99,
+        imageUrl: camisas6,
+      },
+      {
+        id: 7,
+        name: "Camisa 7",
+        value: 89.99,
+        imageUrl: camisas7,
+      },
+      {
+        id: 8,
+        name: "Camisa 8",
+        value: 99.99,
+        imageUrl: camisas8,
+      },
+      {
+        id: 9,
+        name: "Camisa 9",
+        value: 109.99,
+        imageUrl: camisas9,
+      },
+      {
+        id: 10,
+        name: "Camisa 10",
+        value: 119.99,
+        imageUrl: camisas10,
+      },
+    ],
+    carrinho: [],
+    minPrice: "",
+    maxPrice: "",
+    pesquisa: "",
+    ordenacao: 1,
   }
 
+  atualizaMinPreco = (event) => {
+    this.setState({ minPrice: event.target.value })
+  }
+
+  atualizaMaxPreco = (event) => {
+    this.setState({ maxPrice: event.target.value })
+  }
+
+  atualizaPesquisa = (event) => {
+    this.setState({ pesquisa: event.target.value })
+  }
+
+  atualizaOrdenacao = (event) => {
+    this.setState({ ordenacao: event.target.value })
+  }
+
+  removerItem = (event) => {
+    let repetido = false;
+    this.state.carrinho.forEach(camisa => {
+      if (camisa.id === event.target.id && camisa.repeticao > 1) repetido = true
+    })
+
+    if (!repetido) {
+      const novoCarrinho = this.state.carrinho.filter(camisa => {
+        return camisa.id !== event.target.id
+      })
+      this.setState({ carrinho: novoCarrinho });
+    } else {
+      const novoCarrinho = this.state.carrinho.map(camisa => {
+        if (camisa.id === event.target.id) {
+          const auxiliar = camisa.repeticao - 1;
+          const novoObjeto = {
+            ...camisa,
+            repeticao: auxiliar
+          }
+          return novoObjeto
+        } else {
+          return camisa
+        }
+      })
+      this.setState({ carrinho: novoCarrinho });
+    }
+  }
+
+  atualizaCarrinho = (event) => {
+    let repetido = false;
+    this.state.carrinho.forEach(camisa => {
+      if (camisa.id === event.target.id) repetido = true
+    })
+
+    if (!repetido) {
+      const novoObjeto = {
+        id: event.target.id,
+        name: event.target.name,
+        value: event.target.value,
+        repeticao: 1
+      }
+
+      const novoCarrinho = [...this.state.carrinho, novoObjeto]
+      this.setState({ carrinho: novoCarrinho });
+    } else {
+      const novoCarrinho = this.state.carrinho.map(camisa => {
+        if (camisa.id === event.target.id) {
+          const auxiliar = camisa.repeticao + 1;
+          const novoObjeto = {
+            ...camisa,
+            repeticao: auxiliar
+          }
+          return novoObjeto
+        } else {
+          return camisa
+        }
+      })
+      this.setState({ carrinho: novoCarrinho });
+    }
+  }
+
+  render() {
+    return (
+      <Principal>
+        <Header />
+        <Filtro
+          atualizaMinPreco={this.atualizaMinPreco}
+          minPrice={this.state.minPrice}
+          atualizaMaxPreco={this.atualizaMaxPreco}
+          maxPrice={this.state.maxPrice}
+          atualizaPesquisa={this.atualizaPesquisa}
+          pesquisa={this.state.pesquisa}
+          atualizaOrdenacao={this.atualizaOrdenacao}
+          ordenacao={this.state.ordenacao}
+        />
+
+        <MenuCentral>
+          {this.state.camisas.filter(camisa => {
+            // Filtragem valor mínimo
+            return this.state.minPrice === "" || camisa.value >= this.state.minPrice
+          }).filter(camisa => {
+            // Filtragem valor máximo
+            return this.state.maxPrice === "" || camisa.value <= this.state.maxPrice
+          }).filter(camisa => {
+            // Filtragem pelo nome
+            return camisa.name.toLowerCase().includes(this.state.pesquisa.toLowerCase())
+          }).sort((currentJob, nextJob) => {
+            return this.state.ordenacao * (currentJob.value - nextJob.value)
+          }).map(camisa => {
+            return (
+              <Card2>
+                <Imagem src={camisa.imageUrl} />
+                <Titulo>{camisa.name}</Titulo>
+                <Preco><strong>{`R$ ${camisa.value}`}</strong></Preco>
+                <Botao id={camisa.id} name={camisa.name} value={camisa.value} onClick={this.atualizaCarrinho}>Comprar</Botao>
+              </Card2>
+            )
+          })
+          }
+        </MenuCentral>
+
+        <Carrinho
+          carrinho={this.state.carrinho}
+          removerItem={this.removerItem}
+        />
+
+        <Footer />
+      </Principal>
+    );
+  }
 }
---
+++ labecommecer/src/components/carrinho/Carrinho.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const MenuDireita = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: whitesmoke;
+`
+
+const ItensCarrinho = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 90%;
+  margin: 5px;
+`
+
+const BotaoRemover = styled.button`
+`
+
+const ValorTotal = styled.div`
+  width: 90%;
+  margin: 20px 0 0 5px;
+`
+
+export class Carrinho extends Component {
+  render() {
+    let soma = 0
+    return (
+      <MenuDireita>
+        <h3>Carrinho</h3>
+        {this.props.carrinho.map(camisa => {
+          soma = soma + camisa.repeticao * camisa.value;
+          return (
+            <ItensCarrinho >
+              {`${camisa.repeticao}x `}
+              {camisa.name}
+              <BotaoRemover id={camisa.id} onClick={this.props.removerItem}>Excluir</BotaoRemover>
+            </ItensCarrinho>
+          )
+        })}
+
+        <ValorTotal>
+          <strong>
+            {soma !== 0 ? `Valor total: R$ ${soma.toFixed(2)}` : ''}
+          </strong>
+        </ValorTotal>
+      </MenuDireita>
+    )
+  }
+}
